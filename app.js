790c10a8c1e03dbe01591086e713073807e6da18
shafik720 <shafikrasel5@gmail.com>
2022-10-04 15:18:45 +0600

    sdfsf

--- app.js
+++ app.js
@@ -3,8 +3,28 @@ const wrapper = document.querySelector('.wrapper'),
 inputField = wrapper.querySelector('.body-input input'),
 btn = wrapper.querySelector('.app-footer button'),
 infoText = document.querySelector('.body-header-text'),
+locationBtn = document.querySelector('.app-footer button'),
 bodyHeader = document.querySelector('.body-header');
 
+
+
+locationBtn.addEventListener('click',()=>{
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }else{
+        alert("Your browser doesn't support Geolocation")
+    }
+})
+
+function onSuccess(position){
+    const{latitude, longitude} = position.coords;
+    console.log(latitude)
+}
+
+function onError(msg){
+    bodyHeader.classList.add('error');
+    infoText.innerText = msg.message;
+}
 
 inputField.addEventListener('keyup',e=>{
     if(e.key == 'Enter' && inputField.value != ''){
@@ -15,6 +35,7 @@ inputField.addEventListener('keyup',e=>{
 
 function requestApi(city){
     // const key = '3f2f9bc259ce45af21bda8132115c015';
+
     bodyHeader.classList.add('pending');
     infoText.innerText = 'Getting Weather Information....';
     
