f687eb894c449023ec030c295944bb597944a982
shafik720 <shafikrasel5@gmail.com>
2022-10-04 14:56:57 +0600

    sdfsf

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
 const wrapper = document.querySelector('.wrapper'),
 inputField = wrapper.querySelector('.body-input input'),
-btn = wrapper.querySelector('.app-footer button')
+btn = wrapper.querySelector('.app-footer button'),
+infoText = document.querySelector('.body-header-text'),
+bodyHeader = document.querySelector('.body-header');
 
 
 inputField.addEventListener('keyup',e=>{
@@ -13,6 +15,8 @@ inputField.addEventListener('keyup',e=>{
 
 function requestApi(city){
     // const key = '3f2f9bc259ce45af21bda8132115c015';
+    bodyHeader.classList.add('pending');
+    infoText.innerText = 'Getting Weather Information....';
     
     let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3f2f9bc259ce45af21bda8132115c015`;
     
